0b31d0f4c0efc42f00c88ab487a9d8ffa89b5b85
Lily <126211330+l-proulx@users.noreply.github.com>
2023-02-28 12:32:44 -0500

    need to use an old event i think

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023pahat",
+      "defaultValue": "2022pahat",
       "required": "true",
       "disabled": "true"
     },
